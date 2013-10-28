eb7fa0138f2fc2730f458403dcd843a63ee10adf
imarjanovic <imarjanovic@gmail.com>
2013-10-28 13:18:20 -0700

    Created ClassObject and MethodObject classes

---
+++ src/MethodObject.java
@@ -0,0 +1,27 @@
+
+public class MethodObject {
+
+	private String name;
+	private int numberOfLines;
+	
+	public MethodObject(String name, int numberOfLines){
+		this.name = name;
+		this.numberOfLines = numberOfLines;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setNumberOfLines(int numberOfLines){
+		this.numberOfLines = numberOfLines;
+	}
+	
+	public int getNumberOfLines(){
+		return numberOfLines;
+	}
+}
