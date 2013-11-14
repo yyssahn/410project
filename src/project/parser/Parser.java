d1e8bbfd567ee5840011d6aa2192197855a24978
Yoonsung Ahn <yyssahn@hotmail.com>
2013-11-14 12:00:52 -0800

    with the ability to iterate entire source code

--- src/project/parser/Parser.java
+++ src/project/parser/Parser.java
@@ -1,8 +1,13 @@
 package project.parser;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileReader;
+
+import java.io.BufferedReader;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -141,5 +146,46 @@ public class Parser {
 		});
 		return cobj;
 	}
-
+	//loop directory to get file list
+	public static void ParseFilesInDir() throws IOException{
+		File dirs = new File(".");
+		String dirPath = dirs.getCanonicalPath() + File.separator+"code"+File.separator + "TreeFinder" + File.separator;
+ 
+		File root = new File(dirPath);
+		//System.out.println(rootDir.listFiles());
+		File[] files = root.listFiles ( );
+		String filePath = null;
+		String filePathTwo = null;
+		 for (File f : files ) {
+			 filePath = f.getAbsolutePath();
+			 File[] filetwo = f.listFiles();
+			 for (File g : filetwo){
+				 filePathTwo= g.getAbsolutePath();
+				 if (g.isFile()){
+					 parse(readFileToString(filePathTwo));
+					 
+				 }
+			 }
+			 
+		 }
+	}
+	//read file content into a string
+		public static String readFileToString(String filePath) throws IOException {
+			StringBuilder fileData = new StringBuilder(1000);
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));
+	 
+			char[] buf = new char[10];
+			int numRead = 0;
+			while ((numRead = reader.read(buf)) != -1) {
+				System.out.println(numRead);
+				String readData = String.valueOf(buf, 0, numRead);
+				fileData.append(readData);
+				buf = new char[1024];
+			}
+	 
+			reader.close();
+	 
+			return  fileData.toString();	
+		}
+	
 }
